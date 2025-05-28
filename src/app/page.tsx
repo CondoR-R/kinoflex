692aa8d5475a5c18b54697acfaa0be3cb4179742
Dmitriy Latkin <lda020504@gmail.com>
2025-05-28 22:57:35 +0700

    фиксы багов

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ import Carousel from "@/ui/home/carousel/Carousel";
 export default function Home() {
   return (
     <div className={style.home}>
-      <h1 className={style.title}>
-        Откройте для Себя неограниченное колличество контента
-      </h1>
-      <Filters />
+      <div className={style.header}>
+        <h1 className={style.title}>Неограниченное колличество контента</h1>
+        <Filters />
+      </div>
       <Carousel />
     </div>
   );
